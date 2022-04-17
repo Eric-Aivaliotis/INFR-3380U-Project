69642e9a9c539d4efd328c226c70acbc41825a64
Eric-Aivaliotis <55474867+Eric-Aivaliotis@users.noreply.github.com>
2022-04-17 19:47:59 -0400

    Add files via upload

---
+++ FINAL PROJECT SUBMISSION APR18/Interactive scene/Unity files/scripts/button_glow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class button_glow : MonoBehaviour
+{
+    MeshRenderer glowing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        glowing = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {glowing.material.EnableKeyword("_EMISSION"); }
+
+
+    }
+}
